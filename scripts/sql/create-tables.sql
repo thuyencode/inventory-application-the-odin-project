a26ccac5cbb771476652bbbbec05f6ab21031aa0
Thuyen Code <thuyencode@proton.me>
2024-08-05 16:30:43 +0700

    Allow the `description` column to be empty, default image url

--- scripts/sql/create-tables.sql
+++ scripts/sql/create-tables.sql
@@ -10,12 +10,13 @@ CREATE TABLE IF NOT EXISTS
         id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         create_time TIMESTAMP NOT NULL DEFAULT NOW(),
         name VARCHAR(255) UNIQUE NOT NULL,
-        description VARCHAR(500) NOT NULL,
+        description VARCHAR(500),
         price FLOAT NOT NULL,
         stock INT NOT NULL,
         brand VARCHAR(255),
         sku VARCHAR(255) NOT NULL,
         weight INT NOT NULL,
         category_id INT NOT NULL,
+        image_url VARCHAR(255) DEFAULT 'https://dummyjson.com/image/400?text=No+Image&fontFamily=quicksand',
         CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category (id)
     );
