7b9ba794a70a34608ea8a091db17e122fcd549cd
Thuyen Code <thuyencode@proton.me>
2024-08-07 11:50:58 +0700

    Exclude the `./frontend` folder from ESLint in the root project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import ts_eslint from "typescript-eslint";
+import eslint from '@eslint/js'
+import ts_eslint from 'typescript-eslint'
 
 export default ts_eslint.config(
   eslint.configs.recommended,
   ...ts_eslint.configs.recommended,
   {
-    ignores: ["./dist", "./node_modules"],
+    ignores: ['./dist', './node_modules', './frontend']
   }
-);
+)
