e38f52dcdc63c6518fb1a0ca397ee6b945e98f84
Thuyen Code <thuyencode@proton.me>
2024-08-08 21:56:19 +0700

    Use `@vitejs/plugin-react-swr` and run Vite in production mode using `npm run start`

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
 
