0631aed067236f4156fa38d786df335bad4d9834
Thuyen Code <thuyencode@proton.me>
2024-08-10 17:03:10 +0700

    Clean the ESLint configs
    Such a messy config needs an alternative fr fr

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -6,35 +7,35 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import ts_eslint from 'typescript-eslint'
 
-export default ts_eslint.config(
-  {
-    extends: [js.configs.recommended, ...ts_eslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    ignores: ['dist', 'node_modules', 'src/client/components/ui/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2023,
-      globals: globals.browser
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ]
-    }
+export default ts_eslint.config({
+  extends: [
+    js.configs.recommended,
+    ...ts_eslint.configs.recommended,
+    reactPlugin.configs['jsx-runtime'].flat,
+    reactPlugin.configs['recommended'].flat,
+    ...pluginQuery.configs['flat/recommended'],
+    eslintConfigPrettier
+  ],
+  files: ['**/*.{ts,tsx}'],
+  ignores: ['dist', 'node_modules', 'src/client/components/ui/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2023,
+    globals: globals.browser
   },
-  {
-    extends: [eslintConfigPrettier],
-    ignores: ['dist', 'node_modules', 'src/client/components/ui/*.{ts,tsx}'],
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    },
-    ...reactPlugin.configs['jsx-runtime'].flat
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
-)
+})
