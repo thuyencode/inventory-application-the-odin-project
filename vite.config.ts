f41d435fc9118f04036adabd8c3b398ea39d033f
Thuyen Code <thuyencode@proton.me>
2024-08-11 22:02:02 +0700

    Visualize the bundle with `rollup-plugin-visualizer`
    https://github.com/btd/rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), visualizer()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
