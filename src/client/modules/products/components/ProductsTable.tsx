0a9d01a77147c640b34be8c344f103f839581a7e
Thuyen Code <thuyencode@proton.me>
2024-08-12 10:10:48 +0700

    Responsive table

--- src/client/modules/products/components/ProductsTable.tsx
+++ src/client/modules/products/components/ProductsTable.tsx
@@ -6,36 +6,44 @@ interface ProductsTableProps {
 
 function ProductsTable({ products }: ProductsTableProps) {
   return (
-    <div className='w-full overflow-x-auto'>
-      <table className='table-light table table-zebra'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Category</th>
-            <th>Brand</th>
-            <th>Stock</th>
-            <th>Price</th>
-            <th>Weight</th>
-            <th>SKU</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={`${product.name}-${product.id}`}>
-              <th className='table-row-id'>{product.id}</th>
-              <th className='capitalize'>{product.name}</th>
-              <th className='capitalize'>{product.category_name}</th>
-              <th className='capitalize'>{product.brand}</th>
-              <th>{product.stock}</th>
-              <th>${product.price}</th>
-              <th>{product.weight}</th>
-              <th>{product.sku}</th>
+    <>
+      <small className='-my-5 text-center sm:hidden'>
+        Showing the products list in card on small screen devices is recommended
+      </small>
+
+      <div className='w-full overflow-x-auto'>
+        <table className='table-light table table-zebra'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th className='max-sm:hidden'>Category</th>
+              <th className='max-sm:hidden'>Brand</th>
+              <th>Stock</th>
+              <th className='max-sm:hidden'>Price</th>
+              <th className='max-sm:hidden'>Weight</th>
+              <th className='max-sm:hidden'>SKU</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={`${product.name}-${product.id}`}>
+                <th className='table-row-id'>{product.id}</th>
+                <th className='capitalize'>{product.name}</th>
+                <th className='capitalize max-sm:hidden'>
+                  {product.category_name}
+                </th>
+                <th className='capitalize max-sm:hidden'>{product.brand}</th>
+                <th>{product.stock}</th>
+                <th className='max-sm:hidden'>${product.price}</th>
+                <th className='max-sm:hidden'>{product.weight}</th>
+                <th className='max-sm:hidden'>{product.sku}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
