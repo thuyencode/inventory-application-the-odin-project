e52d0bb25e7b2550585cc98f0aa115eba1122cea
Thuyen Code <thuyencode@proton.me>
2024-09-14 18:34:51 +0700

    Close the details menu on mouse event and Esc key

--- src/client/modules/products/components/products-filter/order-by-selector.tsx
+++ src/client/modules/products/components/products-filter/order-by-selector.tsx
@@ -1,35 +1,39 @@
 import { ORDER_BY, ORDER_BY_WITH_ICONS } from '@/shared/constants'
 import { Icon } from '@iconify/react'
 import { Link, useSearch } from '@tanstack/react-router'
+import { forwardRef } from 'react'
+import withClose from './with-close'
 
-function OrderBySelector() {
-  const { orderBy: currentlyOrderBy } = useSearch({ from: '/products' })
+const OrderBySelector = withClose(
+  forwardRef<HTMLDetailsElement>(function (_, ref) {
+    const { orderBy: currentlyOrderBy } = useSearch({ from: '/products' })
 
-  return (
-    <details className='dropdown dropdown-end'>
-      <summary className='btn btn-outline btn-sm m-0 gap-1 capitalize'>
-        Order by
-        {currentlyOrderBy ? `: ${currentlyOrderBy.replace('_', ' ')}` : null}
-      </summary>
+    return (
+      <details className='dropdown dropdown-end' ref={ref}>
+        <summary className='btn btn-outline btn-sm m-0 gap-1 capitalize'>
+          Order by
+          {currentlyOrderBy ? `: ${currentlyOrderBy.replace('_', ' ')}` : null}
+        </summary>
 
-      <ul className='menu dropdown-content menu-md z-[1] mt-2.5 w-min rounded-box border border-base-content/50 bg-base-300 p-2 capitalize shadow'>
-        {ORDER_BY.map((type) => (
-          <li key={type}>
-            <Link
-              className='justify-between text-nowrap'
-              to='.'
-              search={(prev) => ({ ...prev, orderBy: type })}
-              aria-description={`Order by: ${type}`}
-              tabIndex={0}
-            >
-              <Icon className='text-xl' icon={ORDER_BY_WITH_ICONS[type]} />
-              {type.replace('_', ' ')}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </details>
-  )
-}
+        <ul className='menu dropdown-content menu-md z-[1] mt-2.5 w-min rounded-box border border-base-content/50 bg-base-300 p-2 capitalize shadow'>
+          {ORDER_BY.map((type) => (
+            <li key={type}>
+              <Link
+                className='justify-between text-nowrap'
+                to='.'
+                search={(prev) => ({ ...prev, orderBy: type })}
+                aria-description={`Order by: ${type}`}
+                tabIndex={0}
+              >
+                <Icon className='text-xl' icon={ORDER_BY_WITH_ICONS[type]} />
+                {type.replace('_', ' ')}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </details>
+    )
+  })
+)
 
 export default OrderBySelector
--- src/client/modules/products/components/products-filter/sort-in-selector.tsx
+++ src/client/modules/products/components/products-filter/sort-in-selector.tsx
@@ -1,35 +1,39 @@
 import { SORT_IN, SORT_IN_WITH_ICONS } from '@/shared/constants'
 import { Icon } from '@iconify/react'
 import { Link, useSearch } from '@tanstack/react-router'
+import { forwardRef } from 'react'
+import withClose from './with-close'
 
-function SortInSelector() {
-  const { sortIn: currentlySortIn } = useSearch({ from: '/products' })
+const SortInSelector = withClose(
+  forwardRef<HTMLDetailsElement>(function (_, ref) {
+    const { sortIn: currentlySortIn } = useSearch({ from: '/products' })
 
-  return (
-    <details className='dropdown dropdown-end'>
-      <summary className='btn btn-outline btn-sm m-0 gap-1 capitalize'>
-        Sort in
-        {currentlySortIn ? `: ${currentlySortIn}` : null}
-      </summary>
+    return (
+      <details className='dropdown dropdown-end' ref={ref}>
+        <summary className='btn btn-outline btn-sm m-0 gap-1 capitalize'>
+          Sort in
+          {currentlySortIn ? `: ${currentlySortIn}` : null}
+        </summary>
 
-      <ul className='menu dropdown-content menu-md z-[1] mt-2.5 w-min rounded-box border border-base-content/50 bg-base-300 p-2 capitalize shadow'>
-        {SORT_IN.map((type) => (
-          <li key={type}>
-            <Link
-              className='justify-between'
-              to='.'
-              search={(prev) => ({ ...prev, sortIn: type })}
-              aria-description={`Sort in: ${type}`}
-              tabIndex={0}
-            >
-              <Icon className='text-xl' icon={SORT_IN_WITH_ICONS[type]} />
-              {`${type}ending`}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </details>
-  )
-}
+        <ul className='menu dropdown-content menu-md z-[1] mt-2.5 w-min rounded-box border border-base-content/50 bg-base-300 p-2 capitalize shadow'>
+          {SORT_IN.map((type) => (
+            <li key={type}>
+              <Link
+                className='justify-between'
+                to='.'
+                search={(prev) => ({ ...prev, sortIn: type })}
+                aria-description={`Sort in: ${type}`}
+                tabIndex={0}
+              >
+                <Icon className='text-xl' icon={SORT_IN_WITH_ICONS[type]} />
+                {`${type}ending`}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </details>
+    )
+  })
+)
 
 export default SortInSelector
